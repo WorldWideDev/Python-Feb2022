1863ba43514801a6655a6fccbc53afb706bdb716
dev <dev206@gmail.com>
2022-02-14 15:34:18 -0800

    re-organized files, week 2 lecture

---
+++ Week1/Python-Week1Session2/find_duplicates.py
@@ -0,0 +1,50 @@
+"""
+    write a function that takes in a list of numbers
+    return the most commonly found number
+
+    input: [2,5,76,21,2,5,2]
+    out: 2
+
+    input: [2,2,3,3,3,2]
+    out: 3
+"""
+
+def most_common_dupe(numbers): # [2,5,76,21,2,5,2,6]
+    # create empty dictonary to hold on to
+        # each number in "numbers" as a key
+        # a running count of each of those number's occurences in the list
+    numbers_map = {}
+
+    # loop through list of numbers
+    for num in numbers:
+
+        # if 'num' is already a key in our numbers_map
+        if num in numbers_map:
+            # we can increment its value by 1
+            numbers_map[num] += 1
+        # if 'num' is not yet a key
+        else:
+            # we can create a new 'key value pair', giving 1 as its inital value
+            numbers_map[num] = 1
+    
+    # once loop (above) is complete, 
+    #   we have a full mapping of each number and it's number of occcurences
+
+    # now we just need to find which key in this numbers_map dictionary
+    #   contains the largest value
+
+    # we can start by creating a list that contains only the keys
+    numbers_map_keys = list(numbers_map.keys())
+    # from this list of keys, we can grab the first item as our temporary "max_num"
+    max_num = [0]
+    # now we can loop through this list of keys
+    #   to find the key that is mapped to the largest value
+    for key in numbers_map_keys:
+        # if we find that a key's value is larger than our current one
+        if numbers_map[key] > numbers_map[max_num]:
+            # we can update our current max_num
+            max_num = key
+        
+    return max_num 
+
+most_common_dupe([2,5,76,21,2,5,2,6])
---
+++ Week2/Python-Week2Session1/README.md
@@ -0,0 +1,20 @@
+# Python - Week 2 Session 1
+
+# Agenda
+1. Class calendar, assignment & discussion reminders
+2. Review
+    - [Dictionary]((https://login.codingdojo.com/m/309/9251/62273))
+    - [Functions](https://login.codingdojo.com/m/309/9251/62278)
+3. [What is OOP? (Object Oriented Programming)](https://login.codingdojo.com/m/309/9252/62286)
+    - Exploring dictonaries vs defining our own classes
+4. [Classes](https://login.codingdojo.com/m/309/9252/62287), [attributes](https://login.codingdojo.com/m/309/9252/62288) and constructor method
+5. [Methods](https://login.codingdojo.com/m/309/9252/62289)
+    - What is `self`?
+    - [Classmethods and staticmethods](https://login.codingdojo.com/m/309/9252/62293)
+6. [Association of two classes](https://login.codingdojo.com/m/309/9252/62295)
+
+# Resource Links
+1. [Class example](./resources/classExample.png)
+2. [Differences between attributes and methods](./resources/attr_methods.png)
+3. [classmethod example](./resources/class_method.png)
+4. [Association example](./resources/association.png)
