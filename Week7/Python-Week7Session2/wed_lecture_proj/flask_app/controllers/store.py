cc22ea4abffbc09f0dee526783558dd17160209a
Sal Nast <snast@codingdojo.com>
2022-03-24 16:44:30 -0700

    Removing secret keys for Spotify

--- Week7/Python-Week7Session2/wed_lecture_proj/flask_app/controllers/store.py
+++ Week7/Python-Week7Session2/wed_lecture_proj/flask_app/controllers/store.py
@@ -22,8 +22,8 @@ def search_form():
 @app.route("/search", methods=["POST"])
 def search():
     search_term = request.form['search_term']
-    cid = 'f93680415d4d48b8805a8334eb8e7e43'
-    secret = 'ddc2931f3c1742c9a6215804570df7e1'
+    cid = 'PUT YOUR OWN CID'
+    secret = 'PUT YOUR OWN SECRET'
     auth_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(auth_manager=auth_manager)
     results= sp.search(search_term, type="track", limit=50)
@@ -31,8 +31,8 @@ def search():
     return render_template("tracks.html", tracks= results['tracks']['items'])
 @app.route("/tracks")
 def tracks():
-    cid = 'f93680415d4d48b8805a8334eb8e7e43'
-    secret = 'ddc2931f3c1742c9a6215804570df7e1'
+    cid = 'PUT YOUR OWN CID'
+    secret = 'PUT YOUR OWN SECRET'
     auth_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(auth_manager=auth_manager)
     results= sp.search("Beyonce", type="track", limit=50)
